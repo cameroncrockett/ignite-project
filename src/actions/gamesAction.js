d98d320c83ed9b3c2a6994ac1b3ec961c86d0a2f
CameronCrockett <cameroncrockett1@icloud.com>
2021-02-17 16:37:31 -0600

    Update files for project

---
+++ src/actions/gamesAction.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import {
+  popularGamesURL,
+  upcomingGamesURL,
+  newGamesURL,
+  searchGameURL,
+} from "../api";
+
+// Action creator
+
+export const loadGames = () => async (dispatch) => {
+  //Fetch Axios
+  const popularData = await axios.get(
+    popularGamesURL()
+  );
+  const upcomingData = await axios.get(
+    upcomingGamesURL()
+  );
+  const newData = await axios.get(newGamesURL());
+  dispatch({
+    type: "FETCH_GAMES",
+    payload: {
+      popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newData.data.results,
+    },
+  });
+};
+
+export const fetchSearched = (game_name) => async (
+  dispatch
+) => {
+  const searchGames = await axios.get(
+    searchGameURL(game_name)
+  );
+
+  dispatch({
+    type: "FETCH_SEARCHED",
+    payload: {
+      searched: searchGames.data.results,
+    },
+  });
+};
