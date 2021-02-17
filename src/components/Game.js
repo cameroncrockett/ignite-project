4a3314eb80ae1b06ebbe7bb03b98f2e0430ec844
CameronCrockett <cameroncrockett1@icloud.com>
2021-02-17 16:21:07 -0600

    Updated the date format

---
+++ src/components/Game.js
@@ -0,0 +1,70 @@
+//Styling and Animations
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { popup } from "../animations";
+//Redux
+import { useDispatch } from "react-redux";
+import { loadDetail } from "../actions/detailAction";
+// Link
+import { Link } from "react-router-dom";
+//Image resizer
+import { smallImage } from "../util";
+
+const Game = ({ name, released, image, id }) => {
+  const stringPathId = id.toString();
+  // Rearrange Date
+  const rearrangeDate = (date) => {
+    const year = date.slice(0, 4);
+    const newDate = date.slice(5) + "-" + year;
+    return newDate;
+  };
+
+  //  console.log({ released });
+  //Load Detail Handler
+  const dispatch = useDispatch();
+  const loadDetailHandler = () => {
+    document.body.style.overflow = "hidden";
+    dispatch(loadDetail(id));
+  };
+  return (
+    <StyledGame
+      variants={popup}
+      initial="hidden"
+      animate="show"
+      layoutId={stringPathId}
+      onClick={loadDetailHandler}
+    >
+      <Link to={`/game/${id}`}>
+        <motion.h3 layoutId={`title ${stringPathId}`}>
+          {name}
+        </motion.h3>
+        <p>
+          {released.slice(5) +
+            "-" +
+            released.slice(0, 4)}
+        </p>
+        <motion.img
+          layoutId={`image ${stringPathId}`}
+          src={smallImage(image, 640)}
+          alt={name}
+        />
+      </Link>
+    </StyledGame>
+  );
+};
+
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  cursor: pointer;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+  }
+`;
+
+export default Game;
